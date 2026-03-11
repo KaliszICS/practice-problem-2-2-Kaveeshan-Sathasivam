17d8e524805d670eb97fa336a83335aec312a917
Kaveeshan-Sathasivam <343279329@tdsb.ca>
2026-03-11 15:34:59 +0000

    problem2-2Kaveeshan

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,35 @@
+/* 
+File: Lesson 2.2 - More Functions
+Author: Kaveeshan Sathasivam
+Date Created: Mar 11, 2026
+Date Last Modified: Mar 11, 2026
+*/
+
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		
 
-	}
+}
+	public static boolean isEven(int number) {
+    return number % 2 == 0;
+}
 
+	public static boolean isOdd(int number) {
+    return number % 2 != 0;
+
+	
+}
+	public static boolean isPositive(int number) {
+    return number > 0;
+}
+
+	public static boolean isNegative(int number) {
+    return number < 0;
+}
+
+	public static int combinedLength(String str1, String str2) {
+    return str1.length() + str2.length();
+}
 }
